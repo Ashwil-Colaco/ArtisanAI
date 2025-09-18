d691ca59fece3da5d5d2a92b4d28aeaa7efd89f8
Aaditya Ashwin Rao <aadityaashwinrao@gmail.com>
2025-09-18 09:54:42 +0000

    Added DashBoard component

--- ArtisianAI/src/App.jsx
+++ ArtisianAI/src/App.jsx
@@ -1,13 +1,13 @@
-
+import Dashboard from './Components/DashBoard';
 
 function App() {
 
 
   return (
     <>
-      
+      <Dashboard/>
     </>
   )
 }
 
-export default App
+export default App;
